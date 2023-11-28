706757ac4bdcc6e3763d690c4f2d746efd0c6ad7
ahmeteid <ahmeteid355@gmail.com>
2023-11-28 08:48:20 +0300

    added toturial to video_content page and added floatting button is it has a chat inside

--- js/main.js
+++ js/main.js
@@ -16,7 +16,73 @@ window.onclick = function(event) {
     }
 }
 
+function goToLanguage() {
+    window.location.href = "../html/video_content.html"
+}
+
 function watch_bottom() {
     // var watch = document.getElementById("watch");
     window.location.href = "../html/video_content.html"
 }
+
+function openChat(user) {
+    document.getElementById("inbox").style.display = "none";
+    document.getElementById("back-btn").style.display = "block";
+
+
+    const message = getMessages(user);
+
+
+    const chatElement = document.getElementById("chat");
+    chatElement.innerHTML = message.map(message => `<div>${message}</div>`).join('');
+
+    document.getElementById("chat").style.display = "block";
+}
+
+function closeChat() {
+    document.getElementById("back-btn").style.display = "none";
+    document.getElementById("inbox").style.display = "block";
+    document.getElementById("chat").style.display = "none";
+}
+
+function getMessages(user) {
+    const messages = {
+        user1: ["Hello", "How are you?", "Nice to meet you"]
+    };
+    return messages[user] || [];
+}
+
+// Start Floattin Button
+var chatContainer = document.getElementById("chat-container");
+
+function toggleChat() {
+    chatContainer.style.display = chatContainer.style.display === "block" ? "none" : "block";
+}
+
+function closeChat() {
+    chatContainer.style.display = "none";
+}
+
+function sendMessage() {
+    var messageInput = document.getElementById("message-input");
+    var message = messageInput.value;
+
+    if (message.trim() !== "") {
+        var chatDisplay = document.getElementById("chat-display");
+        chatDisplay.innerHTML += "<p><strong>You:</strong> " + message + "</p>";
+
+        // Simulate receiving a fake message
+        setTimeout(function () {
+            chatDisplay.innerHTML += "<p><strong>Friend:</strong> Hi there!</p>";
+            chatDisplay.scrollTop = chatDisplay.scrollHeight;
+        }, 1000);
+
+        // Clear the input field
+        messageInput.value = "";
+
+        // Scroll to the bottom of the chat display
+        chatDisplay.scrollTop = chatDisplay.scrollHeight;
+    }
+}
+
+// End Floattin Button
